e5e4fd9d7a96a51dd45fa271663e04f4f6edbbea
Dhirendra <kumardhiraj609@gmail.com>
2024-05-17 21:58:22 +0530

    resume updated

--- src/components/About.js
+++ src/components/About.js
@@ -30,18 +30,18 @@ const About = () => {
           <div>
             <figure className="text-center">
               <blockquote className="blockquote">
-                <h5 style={{color:theme.textColor}}>
+                <h5 style={{ color: theme.textColor }}>
                   Greetings, I'm Dhirendra Kumar—a passionate web developer,
                   dedicated observer, and contemplative thinker. My journey as a
-                  developer is fueled by an insatiable curiosity and a profound
-                  passion for unraveling the mysteries of the digital world.
+                  developer is driven by an insatiable curiosity and a deep
+                  passion for exploring the digital world.
                 </h5>
-                <p  style={{color:theme.textColor}}>
-                  My toolkit covers a broad range of technologies and languages
-                  for full-stack web development, including HTML, CSS,
-                  JavaScript, React.js,Express.js,MongoDb and Node.js.
-                  Additionally, I'm passionate about mobile app development,
-                  specializing in cross-platform solutions like React Native.
+                <p style={{ color: theme.textColor }}>
+                  As a fresher MCA student, I have a solid foundation in
+                  full-stack web development, with skills in HTML, CSS,
+                  JavaScript, React.js, Express.js, MongoDB, and Node.js.
+                  Additionally, I'm enthusiastic about mobile app development,
+                  with a focus on cross-platform solutions using React Native.
                 </p>
               </blockquote>
             </figure>
